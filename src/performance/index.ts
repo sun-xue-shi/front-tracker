2f7dd46facda8cc95ef7743a3d30371e384fd43f
sun-xue-shi <2595531421@qq.com>
2024-07-11 00:56:30 +0800

    feat:初始化一些性能指标方法

--- src/performance/index.ts
+++ src/performance/index.ts
@@ -1,3 +1,14 @@
+import {
+  CLSMetric,
+  FCPMetric,
+  FIDMetric,
+  LCPMetric,
+  onCLS,
+  onFCP,
+  onLCP,
+} from "web-vitals";
+import { performanceIndexName } from "./types";
+
 export class PerformanceTracker {
   private data: Record<string, Record<string, any>>;
   public trackerInstance: any;
@@ -5,5 +16,35 @@ export class PerformanceTracker {
   constructor(trackerInstance: any) {
     this.data = {};
     this.trackerInstance = trackerInstance;
+  }
+
+  initFCP() {
+    onFCP((metric: FCPMetric) => {
+      this.data[performanceIndexName.FCP] = {
+        name: metric.name,
+        value: metric.value,
+        rating: metric.rating,
+      };
+    });
+  }
+
+  initCLS() {
+    onCLS((metric: CLSMetric) => {
+      this.data[performanceIndexName.CLS] = {
+        name: metric.name,
+        value: metric.value,
+        rating: metric.rating,
+      };
+    });
+  }
+
+  initLCP() {
+    onLCP((metric: LCPMetric) => {
+      this.data[performanceIndexName.LCP] = {
+        name: metric.name,
+        value: metric.value,
+        rating: metric.rating,
+      };
+    });
   }
 }
