128e8dc49116d14bcfe7e84f3cdf16787e0ce059
sun-xue-shi <2595531421@qq.com>
2024-07-20 15:53:32 +0800

    feat:http请求捕获

--- src/action-user/holdFetch.ts
+++ src/action-user/holdFetch.ts
@@ -1,1 +1,36 @@
-export function holdFetch() {}
+import { HttpMetricsData } from "./types";
+
+export function holdFetch(loadHandler: Function) {
+  if ("fetch" in window && typeof window.fetch === "function") {
+    const newFetch = window.fetch;
+    if (!(window as any).newFetch) {
+      (window as any).newFetch = newFetch;
+    }
+
+    (window as any).fetch = async (input: any, init: RequestInit) => {
+      let httpMetricsData = {} as HttpMetricsData;
+
+      httpMetricsData.body = init.body || "";
+      httpMetricsData.method = init.method || "";
+      httpMetricsData.requestTime = new Date().getTime();
+      httpMetricsData.url =
+        (input && typeof input !== "string" ? input?.url : input) || "";
+
+      return newFetch.call(window, input, init).then(async (response) => {
+        const res = response.clone();
+
+        httpMetricsData = {
+          ...httpMetricsData,
+          responseTime: new Date().getTime(),
+          status: res.status,
+          statusText: res.statusText,
+          response: res.text() || JSON.parse(await res.text()),
+        };
+
+        if (typeof loadHandler === "function") loadHandler(httpMetricsData);
+
+        return response;
+      });
+    };
+  }
+}
